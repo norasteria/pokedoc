f39af6dcaed0d473d587e383be506651d5b8e68d
Nora Asteria <nora_asteria@yahoo.co.id>
2020-12-09 11:39:53 +0700

    Change from create-react-app to nextjs project

---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+const Index = () => {
+    return<div>HELLO</div>
+}
+
+export default Index
--- src/config/endpoints.js
+++
@@ -1,7 +0,0 @@
-export default {
-  ability: "/ability",
-  allPokemon: "/pokemon",
-  generation: "/generation",
-  species: "/pokemon-species",
-  types: "/type"
-};
